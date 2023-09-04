deb75c50e787c7b60cb3c86b8700777379eadb09
Colindustrys <mail@colindustrys.com>
2023-09-04 16:33:56 +0200

    movieDetails test finally worls

--- __tests__/api/endpoints.test.js
+++ __tests__/api/endpoints.test.js
@@ -1,14 +1,17 @@
-import { getMovieDetails } from '../../api/endpoints';
-import { getMovieDetailsObject } from '../../api/movieDetails';
+import { getMovieDetails } from "../../api/endpoints";
+import { getMovieDetailsObject } from "../../api/movieDetails";
 
-jest.mock('../../api/movieDetails'); // Mock the module containing getMovieDetailsJsonFromApi
+jest.mock("../../api/movieDetails"); // Mock the module containing getMovieDetailsJsonFromApi
 
-describe('getMovieDetails', () => {
-  it('returns a movie object for a valid ID', async () => {
+describe("getMovieDetails", () => {
+  it("returns a movie object for a valid ID", async () => {
     // Mock the implementation of getMovieDetailsJsonFromApi
-    getMovieDetailsObject.mockResolvedValue(
-      {"_description": "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.", "_id": 238, "_title": "The Godfather"}
-    );
+    getMovieDetailsObject.mockResolvedValue({
+      _description:
+        "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.",
+      _id: 238,
+      _title: "The Godfather",
+    });
 
     const id = 123; // Replace with a valid movie ID for your test case
 
@@ -16,6 +19,11 @@ describe('getMovieDetails', () => {
     const movie = await getMovieDetails(id);
 
     // Assert that the movie object matches the expected result
-    expect(movie).toEqual({"_description": "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.", "_id": 238, "_title": "The Godfather"});
+    expect(movie).toEqual({
+      _description:
+        "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.",
+      _id: 238,
+      _title: "The Godfather",
+    });
   });
 });
