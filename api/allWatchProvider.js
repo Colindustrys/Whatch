a473562b511b1cea1862d4f79876d224265655a3
Colindustrys <mail@colindustrys.com>
2023-09-11 20:12:52 +0200

    nur die top 20 provider holen

--- api/allWatchProvider.js
+++ api/allWatchProvider.js
@@ -1,17 +1,23 @@
 import moviedb from "./movieDbInstance";
 import { parseAllProviders } from "./parser";
 
-// gets the watchprovider for a movie from tmdb and converts it to a list of watch provider objects
+// gets all watchproviders for movies from tmdb and converts it to a list of watch provider objects
 export const getAllWatchProviderObjects = async (id) => {
-  let providerarray
+  let providerarray;
   try {
     //get json from tmdb
-    const res = await moviedb.movieWatchProviderList({ watch_region: "de" })
-    
-    //parse watchprovider obejcts from json
-    providerarray = parseAllProviders(res)
+    const res = await moviedb.movieWatchProviderList({ watch_region: "de" });
 
-    return providerarray;
+    //parse watchprovider obejcts from json
+    const providerarray = parseAllProviders(res);
+
+    //sort by display prio
+    providerarray.sort((a, b) => a.id - b.id);
+
+    //only take the top 20
+    const top20providerarray = providerarray.slice(0, 20);
+
+    return top20providerarray;
   } catch (error) {
     throw error;
   }
