4db0b31ebc872bcfd052e249aa3e2abb4a32d3ad
Colindustrys <mail@colindustrys.com>
2025-07-21 22:37:28 +0200

    fixed ios reads old switch state

--- components/ProviderItem.js
+++ components/ProviderItem.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Platform, Pressable } from "react-native";
+import { Platform, Pressable, AccessibilityInfo } from "react-native";
 import {
   StyledSwitch,
   ProviderItemContainer,
@@ -12,13 +12,28 @@ const ProviderItem = ({
   providerID,
   toggleSwitch,
 }) => {
-  const [localValue, setLocalValue] = useState(providerValue);
-  const isAndroid = Platform.OS === "android";
+  const [localSwitchState, setLocalSwitchState] = useState(providerValue);
+  const [accessibilitySwitchState, setAccessibilitySwitchState] =
+    useState(providerValue);
+  const isIOS = Platform.OS === "ios";
 
   const handleToggle = () => {
-    const newValue = !localValue;
-    setLocalValue(newValue); // Immediate feedback
-    toggleSwitch(providerID, newValue); // Update Redux
+    if (isIOS) {
+      AccessibilityInfo.announceForAccessibility("Stop");
+    }
+    const newSwitchValue = !localSwitchState;
+    setLocalSwitchState(newSwitchValue); // update usestate for Immediate feedback
+    toggleSwitch(providerID, newSwitchValue); // Update Redux
+    setAccessibilitySwitchState(newSwitchValue);
+
+    if (isIOS) {
+      setTimeout(() => {
+        AccessibilityInfo.announceForAccessibility(
+          newSwitchValue ? "on" : "off"
+        );
+      }, 500);
+    } else {
+    }
   };
 
   return (
@@ -26,7 +41,7 @@ const ProviderItem = ({
       accessible={true}
       accessibilityLabel={`Toggle provider ${providerLabel}`}
       accessibilityRole="switch"
-      accessibilityState={{ checked: localValue }}
+      accessibilityState={{ checked: accessibilitySwitchState }}
       accessibilityActions={[{ name: "activate", label: "Toggle" }]}
       onAccessibilityAction={() => {
         handleToggle();
@@ -41,8 +56,8 @@ const ProviderItem = ({
       <StyledSwitch
         accessible={false}
         importantForAccessibility="no"
-        passValue={localValue}
-        value={localValue}
+        passValue={localSwitchState}
+        value={localSwitchState}
         onValueChange={handleToggle}
       />
     </ProviderItemContainer>
